b30edc8db339eedb25538df6a40ab4ca0755cfb2
Tiffany <marthin.t@northeastern.edu>
2021-05-03 20:24:12 -0700

    implement methods for queue

--- directory-tree/queue.c
+++ directory-tree/queue.c
@@ -5,3 +5,40 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "queue.h"
+
+/****** Queue Implementation ******/
+Queue* CreateQueue() {
+    Queue* q = (Queue*)malloc(sizeof(Queue));
+    q->head = NULL;
+    q->size = 0;
+    return q;
+}
+
+void Enqueue(Queue* q, TreeNode* n) {
+    LL_Node* nodeToEnq = CreateLLNode(n);
+    LL_Node* l = AddLast(q->head, nodeToEnq);
+    q->head = l;
+    q->size += 1;
+}
+
+TreeNode* Dequeue(Queue* q) {
+    if (q->head == NULL) return NULL;
+    TreeNode* head = q->head->data;
+    LL_Node* l = q->head;
+    LL_Node* tmp = l->next;
+
+    q->head = tmp;
+    free(l);
+    q->size -= 1;
+    return head;
+}
+
+TreeNode* Peek(Queue* q) {
+    if (q->head == NULL) return NULL;
+    return q->head->data;
+}
+
+int Size(Queue* q) {
+    return q->size;
+}
+
