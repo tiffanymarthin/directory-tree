0ae6450adbbdce68cdff85ee1972f40bdb67b523
Tiffany <marthin.t@northeastern.edu>
2021-05-03 23:07:21 -0700

    add documentation to stack header file

--- directory-tree/stack.h
+++ directory-tree/stack.h
@@ -12,7 +12,27 @@
 
 typedef struct stack Stack;
 
-// Queue implementation using linked list
+// Linked list as the underlying data structure of the stack
 struct stack {
     LL_Node* top;
 };
+
+/****** Stack Implementation ******/
+
+// Create an empty Stack
+Stack* CreateStack();
+
+// Push specified tree node on top of the Stack
+void Push(Stack* stack_data, TreeNode* n);
+
+// Pop the top tree node from top of the Stack
+TreeNode* Pop(Stack* stack_data);
+
+// See the top tree node from Stack without removal
+TreeNode* PeekTop(Stack* stack_data);
+
+// Destroy the Stack
+Stack* DestroyStack(TreeNode* n);
+
+
+#endif
