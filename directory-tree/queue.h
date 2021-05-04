e64476946b74b016b2f61be8aba8dfc2f30bf675
Tiffany <marthin.t@northeastern.edu>
2021-05-03 20:03:16 -0700

    Add documentation to Queue header file

--- directory-tree/queue.h
+++ directory-tree/queue.h
@@ -10,4 +10,33 @@
 #include "tree.h"
 #include "linkedlist.h"
 
+typedef struct queue Queue;
 
+// Queue struct
+struct queue {
+    LL_Node* head;
+    int size;
+};
+
+/****** Queue Method Implementation ******/
+
+// Create an empty Queue
+Queue* CreateQueue();
+
+// Add a TreeNode to the back of the Queue
+void Enqueue(Queue* q, TreeNode* tn);
+
+// Remove the head of the Queue
+TreeNode* Dequeue(Queue* q);
+
+// Returns the head of the Queue without removal
+TreeNode* Peek(Queue* q);
+
+// Destroy the specified Queue
+void DestroyQueue(TreeNode* head);
+
+// Gets the size of the queue
+int Size(Queue* q);
+
+
+#endif
