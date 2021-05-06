7a913d2d56bf3bfe297070084afa97bcf1253d40
Tiffany <marthin.t@northeastern.edu>
2021-05-06 00:46:15 -0700

    Add structs for tree and tree node

--- directory-tree/tree.h
+++ directory-tree/tree.h
@@ -6,3 +6,22 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+
+
+typedef struct treeNode TreeNode;
+
+// Tree node struct
+struct treeNode{
+      char* path;
+      char* name;
+      TreeNode* children;
+      TreeNode* siblings;
+      int level;
+};
+
+typedef struct tree Tree;
+
+// Tree struct
+struct tree{
+      TreeNode* root;
+};
