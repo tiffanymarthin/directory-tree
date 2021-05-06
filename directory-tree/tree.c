500cc5c237e0dc3630e284d478abae1e0836d00f
Tiffany <marthin.t@northeastern.edu>
2021-05-06 00:49:56 -0700

    initial commit and package declaration for tree.c

---
+++ directory-tree/tree.c
@@ -0,0 +1,12 @@
+/**
+ * N-ary tree data structure implementation
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<wchar.h>
+#include<string.h>
+#include "tree.h"
+#include "stack.h"
+#include "queue.h"
