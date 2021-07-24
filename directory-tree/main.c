e095736310aa3de31988bd32905499525aa175ab
Tiffany <marthin.t@northeastern.edu>
2021-07-23 17:36:06 -0700

    Implemented the main logic all in main

--- directory-tree/main.c
+++ directory-tree/main.c
@@ -25,3 +25,122 @@ void PrintDirectoryCounts(int directory_count, int file_count)
     printf("%d ", file_count);
     (file_count > 1) ? printf("files\n") : printf("file\n");
 }
+
+int main()
+{
+    // Starts from the current directory
+    char *dir = ".";
+    struct stat s;
+
+    // Counter for directories and files
+    int dir_ct = 0;
+    int file_ct = 0;
+
+    // Create a root node
+    TreeNode *rootNode = CreateTreeNode(dir, "");
+
+    // Create a Queue with rootNode
+    // This Queue will be used to build the tree
+    Queue *q = CreateQueue();
+    Enqueue(q, rootNode);
+
+    // Building the tree using BFS
+    while (q->head != NULL)
+    {
+        struct dirent **namelist;
+        int num_files;
+
+        TreeNode *currNode = Dequeue(q);
+        // Concat the path with parent's folder path
+        if (currNode->name != "")
+        {
+            char newdir[80];
+            strcpy(newdir, currNode->path);
+            strcat(newdir, "/");
+            strcat(newdir, currNode->name);
+            dir = newdir;
+        }
+
+        // Uses scandir to go through the files in the directory
+        num_files = scandir(dir, &namelist, 0, alphasort);
+
+        // Checks if there is any file
+        if (num_files < 0)
+        {
+            perror("scandir");
+            printf("dir: %s\n", dir);
+            break;
+        }
+
+        // Adding each file/folder to the tree
+        int i = 0;
+        while (i < num_files)
+        {
+            // Ignore dotfiles
+            if (namelist[i]->d_name[0] == '.')
+            {
+                free(namelist[i]);
+                i++;
+                continue;
+            }
+
+            char *directory;
+            char *path;
+            char *filename;
+
+            int length = strlen(dir) + strlen(namelist[i]->d_name);
+            int path_len = strlen(dir);
+            int filename_len = strlen(namelist[i]->d_name);
+
+            directory = (char *)malloc(((length + 3) * sizeof(char)));
+            path = (char *)malloc(((path_len + 3) * sizeof(char)));
+            filename = (char *)malloc(((filename_len + 3) * sizeof(char)));
+
+            if (directory == NULL)
+            {
+                printf("Couldn't malloc for filecrawler.directory\n");
+                return 0;
+                exit(1);
+            }
+
+            // Copy path and filename into malloc'd strings
+            snprintf(directory, length + 3, "%s/%s", dir, namelist[i]->d_name);
+            snprintf(path, path_len + 3, "%s", dir);
+            snprintf(filename, filename_len + 3, "%s", namelist[i]->d_name);
+
+            // Create new node using the path and file name information
+            TreeNode *newNode = CreateTreeNode(path, filename);
+
+            // Add the node to the current rootnode
+            AddChild(currNode, newNode);
+            file_ct++;
+
+            // Use stat() to get info about the given directory or file
+            if (0 == stat(directory, &s))
+            {
+                // If it is a directory, then add to queue to be processed later
+                // Update the file and folder counters
+                if (S_ISDIR(s.st_mode))
+                {
+                    Enqueue(q, newNode);
+                    dir_ct++;
+                    file_ct--;
+                }
+            }
+            // Free the followings as they are copied to malloc'd strings
+            free(namelist[i]);
+            free(directory);
+            i++;
+        }
+        free(namelist);
+    }
+    free(q);
+
+    // Print the tree and the stats
+    PrintTree(rootNode);
+    PrintDirectoryCounts(dir_ct, file_ct);
+
+    DestroyTree(rootNode);
+
+    return 0;
+}
